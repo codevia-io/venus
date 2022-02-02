e7e3ac3895c400fb753cef4f4158f5ee4fa7154a
Vincent Bathelier <53569029+d9beuD@users.noreply.github.com>
2022-02-02 02:01:30 +0100

    Add the ability to set the FastRoute Dispatcher

--- src/Application.php
+++ src/Application.php
@@ -3,10 +3,13 @@
 namespace Codevia\Venus;
 
 use Codevia\Venus\Utils\Http\Input\InputInterface;
+use FastRoute\Dispatcher as FastRouteDispatcher;
 
 class Application
 {
     private InputInterface $inputAdapter;
+    private FastRouteDispatcher $dispatcher;
+
     /**
      * Set the input adapter that corresponds to the format you are working with.
      *
@@ -16,6 +19,18 @@ class Application
     public function setInputAdapter(InputInterface $inputAdapter): self
     {
         $this->inputAdapter = $inputAdapter;
+        return $this;
+    }
+
+    /**
+     * Set the dispatcher with route definitions.
+     *
+     * @param FastRouteDispatcher $dispatcher
+     * @return Application
+     */
+    public function setDispatcher(FastRouteDispatcher $dispatcher): self
+    {
+        $this->dispatcher = $dispatcher;
         return $this;
     }
 
