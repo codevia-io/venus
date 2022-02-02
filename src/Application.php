a3d5612bbb4601052f6d32881f1e89d418249b3c
Vincent Bathelier <53569029+d9beuD@users.noreply.github.com>
2022-02-02 02:00:57 +0100

    Add the ability to set the input adapter

--- src/Application.php
+++ src/Application.php
@@ -2,7 +2,21 @@
 
 namespace Codevia\Venus;
 
+use Codevia\Venus\Utils\Http\Input\InputInterface;
 
 class Application
 {
+    private InputInterface $inputAdapter;
+    /**
+     * Set the input adapter that corresponds to the format you are working with.
+     *
+     * @param InputInterface $inputAdapter
+     * @return Application
+     */
+    public function setInputAdapter(InputInterface $inputAdapter): self
+    {
+        $this->inputAdapter = $inputAdapter;
+        return $this;
+    }
+
 }
