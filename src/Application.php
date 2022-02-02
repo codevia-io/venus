517bc840762fcd43511324567547d0ba75f94f18
Vincent Bathelier <53569029+d9beuD@users.noreply.github.com>
2022-02-02 02:01:56 +0100

    Add the run process

--- src/Application.php
+++ src/Application.php
@@ -3,7 +3,13 @@
 namespace Codevia\Venus;
 
 use Codevia\Venus\Utils\Http\Input\InputInterface;
+use Codevia\Venus\Utils\RequestHandler;
 use FastRoute\Dispatcher as FastRouteDispatcher;
+use Laminas\Diactoros\ServerRequestFactory;
+use Middlewares\ErrorFormatter\JsonFormatter;
+use Middlewares\ErrorHandler;
+use Middlewares\Utils\Dispatcher;
+use Middlewares\Utils\RequestHandlerContainer;
 
 class Application
 {
@@ -34,4 +40,36 @@ class Application
         return $this;
     }
 
+    /**
+     * Execute the application logic
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        $request = ServerRequestFactory::fromGlobals(
+            $_SERVER,
+            $_GET,
+            $this->inputAdapter::getParsedBody(),
+            $_COOKIE,
+            $_FILES
+        );
+        $container = new RequestHandlerContainer();
+
+        $session = new \Middlewares\PhpSession();
+        $session->name('VENUSSESSID')
+            ->regenerateId(60); // Prevent session fixation attacks
+
+        $dispatcher = new Dispatcher([
+            new \Middlewares\Emitter(),
+            new ErrorHandler([
+                new JsonFormatter()
+            ]),
+            $session,
+            (new \Middlewares\FastRoute($this->dispatcher))->attribute('handler'),
+            (new RequestHandler($container))->handlerAttribute('handler'),
+        ]);
+
+        $dispatcher->dispatch($request);
+    }
 }
